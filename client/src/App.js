c46c7523aae3bd4d88aaf0e0240b6cb3a6dbc86e
Mryesiller <gokselyesiller@gmail.com>
2022-06-18 17:06:23 +0300

    aws integration done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React from "react"
+import "./App.css"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import OtherPage from "./OtherPage"
+import Fib from "./Fib"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Welcome fib calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
@@ -28,7 +19,7 @@ function App() {
         </div>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
